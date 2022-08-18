034418e85841e03c2366c7b3408560ec9d48c377
Ruthiel <ruthiel88@protonmail.com>
2022-08-18 12:14:07 +0100

    Add Passenger model

--- flights/models.py
+++ flights/models.py
@@ -9,6 +9,7 @@ class Airport(models.Model):
     def __str__(self) -> str:
         return f"{self.city} ({self.code})"
 
+
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
@@ -16,3 +17,12 @@ class Flight(models.Model):
 
     def __str__(self) -> str:
         return f"{self.id}: {self.origin} to {self.destination}"
+
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self) -> str:
+        return f"{self.first} {self.last}"
