da3b336dc3f0063fb8095962ad03b8fd500c1bff
Ruthiel <ruthiel88@protonmail.com>
2022-08-18 15:10:49 +0100

    Add authentication logic

--- flights/views.py
+++ flights/views.py
@@ -1,14 +1,42 @@
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
 
 from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+
+def auth_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "flights/login.html", {
+                "message":"Invalid credentials"
+            })
+        
+    return render(request, "flights/login.html")
+
+
+def auth_logout(request):
+    logout(request)
+    return render(request, "flights/login.html", {
+        "message":"Logged out."
+    })
+
 
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
@@ -17,6 +45,7 @@ def flight(request, flight_id):
         "passengers": flight.passengers.all(),
         "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
 
 def book(request, flight_id):
     if request.method == "POST":
