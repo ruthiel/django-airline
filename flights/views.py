21573cdac8a216faa6335a997e9e37cb541df09e
Ruthiel <ruthiel88@protonmail.com>
2022-08-18 13:55:06 +0100

    Add selection button to choose non_passengers to a flight

--- flights/views.py
+++ flights/views.py
@@ -15,12 +15,12 @@ def flight(request, flight_id):
     return render(request, "flights/flight.html", {
         "flight": flight,
         "passengers": flight.passengers.all(),
-        "non_passengers": Passenger.objects.exclude(flight=flight).all()
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 def book(request, flight_id):
-    if request.method == "post":
+    if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
         passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
         passenger.flights.add(flight)
-        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
